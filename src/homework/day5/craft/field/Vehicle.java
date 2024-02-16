6ae88b357e4f4c921f037e2ad4a21a946a97805d
Vera Shyrokikh <vshyrokikh@gmail.com>
2024-02-16 04:54:11 +0200

    playgrount task1

---
+++ src/homework/day5/craft/field/Vehicle.java
@@ -0,0 +1,20 @@
+package homework.day5.craft.field;
+
+import homework.day5.playground.essence.Matter;
+
+public class Vehicle extends Matter {
+    protected String name;
+
+    public Vehicle(int mass, String name) {
+        super(mass);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
